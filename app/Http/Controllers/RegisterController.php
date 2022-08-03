d5aedf094d666e90f2c8fbf4b856c7b5b9df860e
MiroslavZaprazny <miro.zaprazny8@gmail.com>
2022-08-03 13:18:52 +0200

    Register validation

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,18 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
     public function create(Request $request)
     {
+        $validate = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|confirmed|min:4',
+            'password_confirmation' => 'required|min:4'
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json([
+                'validate_err' => $validate->messages()
+            ]);
+        }
+
         User::create([
             'email' => $request->email,
-            'password' => $request->password
+            'password' => bcrypt($request->password)
         ]);
         return [
-            'status' => 'ok',
-            'message' => 'User was added successfully'
+            'status' => '200',
+            'message' => 'Your account was created!'
         ];
     }
 }
